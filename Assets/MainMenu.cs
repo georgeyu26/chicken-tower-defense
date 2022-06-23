4442c96e11bcbcbce0d0e74aa773079e9e8625f1
simenghe <simengschool@gmail.com>
2022-06-23 17:15:00 -0400

    Added Main Menu with Play + Quit (#6)
    * Play Button For Menu added
    
    * Added Quit Button

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("GAME IS QUITTING");
+        Application.Quit();
+    }
+
+    // public void Start()
+    // {
+    // }
+    //
+    // // Update is called once per frame
+    // void Update()
+    // {
+    // }
+}
