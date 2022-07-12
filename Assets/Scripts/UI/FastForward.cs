b9f1d36917985c96b7254b488141550acfe3001e
simenghe <simengschool@gmail.com>
2022-07-12 17:28:18 -0400

    Simeng/fast foward (#34)
    * Menu Button
    
    * Fast forward

---
+++ Assets/Scripts/UI/FastForward.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastForward : MonoBehaviour
+{
+    public static bool FastForwarded;
+
+    public void ToggleFastForward()
+    {
+        if (PauseMenu.paused)
+        {
+            return;
+        }
+        if (FastForwarded)
+        {
+            Time.timeScale = 1f;
+            FastForwarded = false;
+        }
+        else
+        {
+            Time.timeScale = 2f;
+            FastForwarded = true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
