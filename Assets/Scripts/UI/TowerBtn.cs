783e5c243b14203a2c33bf970fe4cabae822a543
Conan-Wang <wangc2736@gmail.com>
2022-07-11 20:02:14 -0400

    disable enable towerBtn based on money

--- Assets/Scripts/UI/TowerBtn.cs
+++ Assets/Scripts/UI/TowerBtn.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TowerBtn : MonoBehaviour
 {
+    private GameManager gameManager;
+
+    [SerializeField]
+    private Button towerBtn;
+
     [SerializeField]
     private GameObject towerPrefab;
 
@@ -16,6 +22,19 @@ public class TowerBtn : MonoBehaviour
     public int Cost{
         get{
             return cost;
+        }
+    }
+
+    private void Awake(){
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void Update(){
+        if(gameManager.Currency < this.cost){
+            towerBtn.interactable = false;
+        }
+        else{
+            towerBtn.interactable = true;
         }
     }
     
