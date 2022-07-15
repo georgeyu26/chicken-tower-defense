34e7e0affc441f68681b6c1886fc5f9c8bb181ea
ymalik2000 <ymalik2000@gmail.com>
2022-07-15 19:34:48 -0400

    Added shop data for each tower

---
+++ Assets/Scripts/Entity/ShopData.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public int cost;
+
+    private GameManager gameManager;
+
+    public int level;
+
+    public int upgradeCost;
+    public int repairCost;
+    void Start()
+    {
+        level = 1;
+        upgradeCost = 100;
+        repairCost = 100;
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
