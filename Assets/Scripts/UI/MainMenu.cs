2259ca557a013c64889171eeb57ef9f77186480f
simenghe <simengschool@gmail.com>
2022-07-17 17:08:11 -0400

    Changes to load game (#50)

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -15,4 +15,9 @@ public class MainMenu : MonoBehaviour
         Debug.Log("GAME IS QUITTING");
         Application.Quit();
     }
+
+    public void LoadGame()
+    {
+        Debug.Log("LoadGame Attempted");
+    }
 }
