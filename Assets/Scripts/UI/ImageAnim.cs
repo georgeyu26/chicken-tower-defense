ee769afcbdc7eb9784b68dd21db0429697bf4212
Jerry Yang <jerry.yang9557@gmail.com>
2022-07-18 22:14:27 -0400

    changed update to fixed update in ImageAnim.cs

--- Assets/Scripts/UI/ImageAnim.cs
+++ Assets/Scripts/UI/ImageAnim.cs
@@ -17,7 +17,7 @@ public class ImageAnim : MonoBehaviour {
 		image = GetComponent<Image> ();
 	}
 
-	void Update () {
+	void FixedUpdate () {
 		if (!loop && index == sprites.Length) return;
 		frame ++;
 		if (frame < framesPerSprite) return;
